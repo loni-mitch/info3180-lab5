1b54294159775fa0de231d31108fc0dac18d250a
Loni mitchell <loni.mitchell@mymona.uwi.edu>
2018-03-11 04:31:23 +0000

    step 8

--- app/views.py
+++ app/views.py
@@ -34,21 +34,30 @@ def login():
     if request.method == "POST":
         # change this to actually validate the entire form submission
         # and not just one field
-        if form.username.data:
+        username = form.username.data
+        password = form.password.data
+        user = UserProfile.query.filter_by(username = username, password = password).first()
+        if user is not None:
             # Get the username and password values from the form.
 
             # using your model, query database for a user based on the username
             # and password submitted
             # store the result of that query to a `user` variable so it can be
             # passed to the login_user() method.
+            remember_me = False
+            if 'remember_me' in request.form:
+                remember_me = True
 
             # get user id, load into session
-            login_user(user)
+            login_user(user, remember = remember_me)
+            flash('Logged in successfully!', 'success')
+            next_page = request.args.get('next')
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            return redirect(url_for('secure_page') or next_page)  # they should be redirected to a secure-page route instead
+        else:
+            flash('The Username or Password is incorrect.', 'warning')
     return render_template("login.html", form=form)
-
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
